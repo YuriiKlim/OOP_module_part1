5fe1907ae79864d1ed35334810013e9242b8bb11
Coon_potaskun <35497588+YuriiKlim@users.noreply.github.com>
2024-03-12 20:30:20 +0200

    четверте завдання

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-class Dogs():
-    def __init__(self, name, age, breed):
-        self.name = name
-        self.age = age
-        self.breed = breed
+# Завдання 4
+#  Створіть клас Car з атрибутами brand (марка
+# автомобіля), model (модель) та year (рік випуску).
+# Додайте метод start_engine, який виведе повідомлення
+# про те, що двигун запущено.
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
 
+    def start_engine(self):
+        if self.year > 1980:
+            print(f"Двигун {self.brand} {self.model} {self.year} року запущено.")
+        else: print("винь-винь-винь, не завівся =(")
 
-# створення екземпляру класу
-my_dog = Dogs("Пес", 5, "хаскі")
-print(my_dog.name)
-print(my_dog.age)
-print(my_dog.breed)
+my_car = Car("Toyota", "Camry", 2022)
+my_car.start_engine()
+
