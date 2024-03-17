81a0c8e3274a0b2ae4ff744bbe3fb5f27844310a
Coon_potaskun <35497588+YuriiKlim@users.noreply.github.com>
2024-03-17 17:48:34 +0200

    Завдання 1

---
+++ HW2_task1.py
@@ -0,0 +1,88 @@
+# Завдання 1
+# До вже реалізованого класу «Людина» додайте конструктор та необхідні перевантажені методи.
+import datetime
+import re
+
+class Person:
+    def __init__(self, full_name="", date_of_birth="", phone="", city="", country="", address=""):
+        self.full_name = full_name
+        self.date_of_birth = datetime.datetime.strptime(date_of_birth, "%d.%m.%Y").date() if date_of_birth else None
+        self.phone = phone
+        self.city = city
+        self.country = country
+        self.address = address
+
+    def input_alpha_data(self, prompt, field_name):
+        while True:
+            value = input(prompt)
+            if value.isalpha() or " " in value:
+                setattr(self, field_name, value)
+                break
+            else:
+                print("Неправильне значення. Будь ласка, введіть лише букви.")
+
+    def input_date_of_birth(self):
+        while True:
+            date_input = input("Введіть дату народження (дд.мм.рррр): ")
+            try:
+                self.date_of_birth = datetime.datetime.strptime(date_input, "%d.%m.%Y").date()
+                break
+            except ValueError:
+                print("Невірний формат дати. Спробуйте ще раз.")
+
+    def input_phone(self):
+        phone_pattern = r"^\+\d+$"
+        while True:
+            phone_input = input("Введіть контактний телефон у форматі '+12345678': ")
+            if re.match(phone_pattern, phone_input):
+                self.phone = phone_input
+                break
+            else:
+                print("Невірний формат телефонного номера. Спробуйте ще раз.")
+
+    def input_data(self):
+        self.input_alpha_data("\nВведіть ПІБ: ", "full_name")
+        self.input_date_of_birth()
+        self.input_phone()
+        self.input_alpha_data("Введіть місто: ", "city")
+        self.input_alpha_data("Введіть країну: ", "country")
+        self.address = input("Введіть домашню адресу: ")
+
+    def display_data(self):
+        print("\nДані про людину:")
+        print(f"ПІБ: {self.full_name}")
+        dob = self.date_of_birth.strftime('%d.%m.%Y') if self.date_of_birth else "Не вказано"
+        print(f"Дата народження: {dob}")
+        print(f"Контактний телефон: {self.phone}")
+        print(f"Місто: {self.city}")
+        print(f"Країна: {self.country}")
+        print(f"Домашня адреса: {self.address}")
+
+    def __lt__(self, other):
+        if self.date_of_birth and other.date_of_birth:
+            return self.date_of_birth > other.date_of_birth
+        return False
+
+    def __eq__(self, other):
+        return len(self.full_name) == len(other.full_name)
+
+    def compare_names(self, other):
+        return len(self.full_name) > len(other.full_name)
+
+    def city_first_alphabetically(self, other):
+        return self.city < other.city
+
+    def country_first_alphabetically(self, other):
+        return self.country < other.country
+
+person1 = Person()
+person2 = Person()
+person1.input_data()
+person2.input_data()
+person1.display_data()
+person2.display_data()
+
+print("Хто старший:", f"{person1.full_name}" if person2 < person1 else f"{person2.full_name}")
+print("У кого довший ПІБ:", f"{person1.full_name}" if person1.compare_names(person2) else f"{person2.full_name}")
+print("Чиє місто йде першим у алфавітному порядку:", f"{person1.full_name}" if person1.city_first_alphabetically(person2) else f"{person2.full_name}")
+print("Чия країна йде перша у алфавітному порядку:", f"{person1.full_name}" if person1.country_first_alphabetically(person2) else f"{person2.full_name}")
