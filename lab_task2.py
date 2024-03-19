564e00ec88e6eae6f9f12455da6f395eaf3feaff
Coon_potaskun <35497588+YuriiKlim@users.noreply.github.com>
2024-03-19 20:46:07 +0200

    Завдання 2 добавив кількість товарів

--- lab_task2.py
+++ lab_task2.py
@@ -7,16 +7,16 @@ class ShoppingCart:
     def __init__(self):
         self.__items = []
 
-    def add_item(self, name, price):
+    def add_item(self, name, count, price):
         """Додає товар у кошик."""
-        if isinstance(name, str) and isinstance(price, (int, float)) and price > 0:
-            self.__items.append({"name": name, "price": price})
+        if isinstance(name, str) and isinstance(price, (int, float)) and price > 0 and isinstance(count, int) and count > 0:
+            self.__items.append({"name": name, "price": price, "count": count})
         else:
             print("Некоректні дані товару.")
 
     def get_total_price(self):
         """Підраховує загальну вартість товарів у кошику."""
-        return sum(item["price"] for item in self.__items)
+        return sum(item["price"] * item["count"] for item in self.__items)
 
     def display_items(self):
         """Виводить список товарів у кошику та загальну вартість."""
@@ -25,12 +25,12 @@ class ShoppingCart:
             return
         print("Товари у кошику:")
         for item in self.__items:
-            print(f"- {item['name']}: {item['price']} грн")
+            print(f"- {item['name']} {item['count']}: {item['price']} грн")
         print(f"Загальна вартість: {self.get_total_price()} грн")
 
 cart = ShoppingCart()
-cart.add_item("Книга", 300)
-cart.add_item("Ручка", 15)
-cart.add_item("Зошит", 20)
+cart.add_item("Книга", 2,300)
+cart.add_item("Ручка", 5, 15)
+cart.add_item("Зошит", 3,20)
 
 cart.display_items()
